fca270b1e15c6e3bc57cc5189c9aab7df7c94d20
satyainv <satyainv459@gmail.com>
2022-12-03 01:49:50 +0000

    Initial commit

---
+++ src/main/java/org/example/DevClass.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class DevClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DevClass" );
+    }
+}
---
+++ src/test/java/org/example/DevTestClass.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DevTestClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DevTestClass" );
+    }
+}
